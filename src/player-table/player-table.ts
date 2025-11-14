d80adb4d168a4cd4cf1e5c864a1d6a261bfd9e9c
mizutismask <severinek@gmail.com>
2025-11-14 15:23:10 +0100

    refacto: remove duplicate player name and anchor

--- src/player-table/player-table.ts
+++ src/player-table/player-table.ts
@@ -8,9 +8,7 @@ class PlayerTable {
 		const isMyTable = player.id === game.getPlayerId()
 		const ownClass = isMyTable ? 'own' : ''
 		let html = `
-			<a id="anchor-player-${player.id}"></a>
             <div id="player-table-${player.id}" class="player-order${player.playerNo} player-table ${ownClass}">
-				<span class="player-name" style="color:#${player.color}">${player.name}</span>
             </div>
         `
 		dojo.place(html, 'player-tables')
