ea61f1a32d3e2ef587ac25f58adbdb928dd2d911
mizutismask <severinek@gmail.com>
2026-02-23 13:41:01 +0100

    fix: ending a turn by taking a card with extra discovery action was causing irrelevant message "are you sure you want to proceed without taking any discovery"

--- modules/php/States/PlayerTurn.php
+++ modules/php/States/PlayerTurn.php
@@ -71,7 +71,7 @@ class PlayerTurn extends GameState {
             "canPass" => $mandatoryMoveDone,
             "canResetTurn" => $mandatoryMoveDone,
             "currentFishAction" => $this->game->globals->get(Constants::GLBL_CURRENT_FISH_ACTION),
-            "possibleSlotsForDiscovery" => $mandatoryMoveDone  && $discoveryTaken == false || $this->globals->get(Constants::GLBL_CURRENT_FISH_ACTION,) == "D" ? $this->game->islandMgr->getPossibleSlotsForDiscovery() : [],
+            "possibleSlotsForDiscovery" => $mandatoryMoveDone && ($discoveryTaken == false || $this->globals->get(Constants::GLBL_CURRENT_FISH_ACTION,) == "D") ? $this->game->islandMgr->getPossibleSlotsForDiscovery() : [],
             "crossedSlots" => $this->game->islandMgr->getPossibleSlotsForDiscovery(),
             "remainingTreasures" => $this->globals->get(Constants::GLBL_REMAINING_TREASURES, 0),
             "canTradeFishForMove" => FISH_ACTION_COST["M"] <= $this->game->playerFishCounter->get($this->game->getMostlyActivePlayerId()),
@@ -135,6 +135,10 @@ class PlayerTurn extends GameState {
                     'player_name' => $this->game->getPlayerName($activePlayerId),
                 ]);
                 $this->playInstantCard($activePlayerId, $card);
+            }
+
+            if ($fishAction == "D") {
+                $fishAction = $this->globals->set(Constants::GLBL_CURRENT_FISH_ACTION, null);
             }
         } else {
             /*$typedSlot = $this->game->getCatSlotFromGlobalSlot($slot);
